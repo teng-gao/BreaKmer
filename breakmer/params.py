90bbf21d492fd8e4c551bb0928a9b0475bff98bf
ryan.abo@gmail.com <ryan.abo@gmail.com>
2016-05-11 15:07:09 -0400

    Parameter module code was cleaned and documented.

--- breakmer/params.py
+++ breakmer/params.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-# import logging 
+import subprocess
 import breakmer.utils as utils
 
 __author__ = "Ryan Abo"
@@ -12,7 +12,8 @@ __email__ = "ryanabo@gmail.com"
 __license__ = "MIT"
 
 
-class ParamManager:
+class ParamManager(object):
+
     """ParamManager class stores all the input specifications provided to the program to run. These include
     file paths, thresholds, directories, etc...
 
@@ -25,10 +26,10 @@ class ParamManager:
     """
 
     def __init__(self, arguments):
-        """Initialize ParamManager class.
+
+        """Initialize the ParamManager class.
 
         Args:
-            fncCmd (str):       The command to execute - run / prepare_reference_data / start_blat_server.
             arguments (dict):   The argparse dictionary object from the command line parameters.
         Returns:
             None
@@ -36,37 +37,41 @@ class ParamManager:
             None
         """
 
-        self.loggingName = 'breakmer.params'
+        self.logging_name = 'breakmer.params'
         self.opts = {}
         self.filter = None
         self.targets = {}
         self.paths = {}
-        # self.fncCmd = arguments.fncCmd
-        self.set_params(arguments)
+        self.fnc_cmd = arguments.fnc_cmd
+        self.input_args = arguments
+        self.set_params()
 
-    def set_params(self, arguments):
+    def set_params(self):
+
         """Organize and format all input parameters into class variables to access
-        later. Specific instances of parameters are checked and set. All parameters that are
+        later. Specific instances of parameters are checked and set. All the parameters that are
         set are logged. The target objects are set along with the paths.
+
         Args:
-            arguments (dict): The argparse dictionary object from the command line options.
+            None
         Returns:
             None
         Raises:
             None
         """
-        # print arguments
-        # Check if analysis directory is set to use for logging file.
-        utils.setup_logger(arguments.analysis_dir, 'breakmer')  # Create logging object.
-        self.parse_opts(arguments)  # Parse the config file and command line parameters into the self.opts dictionary.
 
-        utils.log(self.loggingName, 'info', 'Setting up parameters')
+        log_msgs = self.parse_opts()  # Parse the config file and command line parameters into the self.opts dictionary.
+        utils.setup_logger(self.get_param('analysis_dir', True), 'breakmer')  # Create logging object.
+        # Log the parameter setup after the logging object is created.
+        utils.log(self.logging_name, 'info', 'Setting up parameters')
 
         # Log all parameters passed in, warn for poor paths
-        for paramKey, paramValue in self.opts.items():
-            utils.log(self.loggingName, 'info', '%s = %s' % (paramKey, paramValue))
+        for param_key, param_value in self.opts.items():
+            utils.log(self.logging_name, 'info', '%s = %s' % (param_key, param_value))
+        # Log parameter overwritten by configuration file input values.
+        for log_msg in log_msgs:
+            utils.log(self.logging_name, log_msg[0], log_msg[1])
 
-        # sys.exit(2)
         self.set_targets()
         self.paths['ref_data'] = os.path.abspath(os.path.normpath(self.opts['reference_data_dir']))  # Path to target reference sequence fast files.
         self.set_param('reference_fasta_dir', os.path.split(self.opts['reference_fasta'])[0])  # Path to genome fasta file.
@@ -81,20 +86,20 @@ class ParamManager:
 
         # Create all the paths.
         for path in self.paths:
-            utils.log(self.loggingName, 'info', 'Creating %s directory (%s)' % (path, self.paths[path]))
+            utils.log(self.logging_name, 'info', 'Creating %s directory (%s)' % (path, self.paths[path]))
             if not os.path.exists(self.paths[path]):
                 os.makedirs(self.paths[path])
 
         # If starting the blat server then return.
-        # if self.fncCmd == 'start_blat_server':
-        #     utils.log(self.loggingName, 'info', 'Starting the blat server.')
-        #     return
+        if self.fnc_cmd == 'start_blat_server':
+            utils.log(self.logging_name, 'info', 'Starting the blat server.')
+            return
 
         # self.check_binaries()  # Check if Jellyfish and Cutadapt work.
         # self.filter = resultfilter.ResultFilter(self.get_param('filterList'), self)  # Instantiate the filter class.
         # self.set_insertsize_thresh()  # Set the expected insert size threshold from the properly mapped read
 
-    def parse_opts(self, arguments):
+    def parse_opts(self):
         """Formats input parameters into self.opts dictionary. It first parses the configuration file and stores the key, values in the self.opts dictionary.
 
         It will exit with an error if the configuration file does not have lines in the proper format (i.e., key=value).
@@ -102,52 +107,56 @@ class ParamManager:
         A final check is performed for the required parameters depending on the parameters that have been passed.
         
         Args:
-            arguments (dict):   The argparse dictionary object from the command line options.
+            None
         Returns:
             None
         Raises:
             None
         """
 
-        for line in open(arguments.config_fn, 'rU'):
+        for line in open(self.input_args.config_fn, 'rU'):
             line = line.strip()
             if line == '' or line.find('#') > -1:  # Allow for blank lines and comments
                 continue
             linesplit = line.split("=")
             if len(linesplit) == 1:  # Make sure the lines in the configuration file are set properly.
-                err_msg = 'Config line', line, ' not set correctly. Exiting.'
+                err_msg = 'Configuration file line', line, ' is not set correctly. Exiting.'
                 print err_msg
-                # utils.log(self.loggingName, 'error', err_msg)
+                # utils.log(self.logging_name, 'error', err_msg)
                 sys.exit(1)
             else:
                 key, value = linesplit
                 self.set_param(key, value)  # Store key-value in opts dictionary.
 
+        log_msgs = []
         # Store all the arguments into the self.opts dictionary.
-        for opt in vars(arguments):
-            if (self.get_param(opt) is not None) and (vars(arguments)[opt] is None):
+        for opt in vars(self.input_args):
+            if (self.get_param(opt) is not None) and (vars(self.input_args)[opt] is None):
                 # Save log message
-                # utils.log(self.loggingName, 'info', 'Parameter %s is set in config file and not on the command line. Using config file value %s.' % (opt, self.get_param(opt)))
+                # utils.log(self.logging_name, 'info', 'Parameter %s is set in config file and not on the command line. Using config file value %s.' % (opt, self.get_param(opt)))
+                log_msgs.append(('info', 'Parameter %s is None in input parameters and set in configuration file. Using configuration file value %s for analysis.' % (opt, self.get_param(opt))))
             else:
-                self.set_param(opt, vars(arguments)[opt])
+                self.set_param(opt, vars(self.input_args)[opt])
 
         # Check that the required parameters are set.
-        # required = ['analysis_name',
-        #             'targets_bed_file',
-        #             'sample_bam_file',
-        #             'analysis_dir',
-        #             'reference_data_dir',
-        #             'cutadapt_config_file',
-        #             'reference_fasta',
-        #             'gene_annotation_file']
-        # if self.fncCmd == 'prepare_reference_data':
-        #     required = ['reference_data_dir', 'reference_fasta', 'targets_bed_file']
+        required = ['analysis_name',
+                    'targets_bed_file',
+                    'sample_bam_file',
+                    'analysis_dir',
+                    'reference_data_dir',
+                    'cutadapt_config_file',
+                    'reference_fasta',
+                    'gene_annotation_file']
+        if self.fnc_cmd == 'prepare_reference_data':
+            required = ['reference_data_dir', 'reference_fasta', 'targets_bed_file']
 
-        # for req in required:
-        #     self.get_param(req, True)
+        for req in required:
+            self.get_param(req, True)
         # Return log messages that need to be back logged once the logger is setup
+        return log_msgs
 
     def set_targets(self):
+
         """Parse the targets bed file and store them in a dictionary. Limit to a gene
         list if input.
 
@@ -157,10 +166,13 @@ class ParamManager:
         four columns (chromosome, start, end, name) with an optional fourth column
         containing a coding feature (i.e., exon or intron). Each row is either a tiled
         region with sequencing coverage or it is just a region to analyze by BreaKmer.
+        
         The name can be applied to multiple rows, and if multiple tiled regions are input
         with the same name they are aggregated together under the same key.
+        
         Store the target information in the self.target dictionary with the name as the key
         and a list of tuples of interval genomic locations as the values.
+
         self.target[gene_name] = [(chrom, start_bp, end_bp, name, feature),...]
 
         Args:
@@ -172,33 +184,32 @@ class ParamManager:
         """
 
         # Get the gene list file path if it exists.
-        geneList = self.get_param('gene_list')
-        regionList = None
-        if geneList:
-            regionList = []
+        gene_list = self.get_param('gene_list')
+        region_list = None
+        if gene_list:
+            region_list = []
             # Each line contains a gene name.
-            for line in open(geneList, 'r'):
-                regionList.append(line.strip().upper())
+            for line in open(gene_list, 'r'):
+                region_list.append(line.strip().upper())
 
-        utils.log(self.loggingName, 'info', 'Parsing target list')
+        utils.log(self.logging_name, 'info', 'Parsing target list')
 
         # TODO: Check to make sure there aren't duplicate genes.
-        # cur_region = ['', []]
-        for target in open(self.get_param('targets_bed_file'), 'rU'):
-            # Each target is formatted like a bed, chr bp1 bp2 name
-            target = target.strip()
-            targetsplit = target.split()
-            chrm, bp1, bp2, name = targetsplit[0:4]
-            if regionList:
-                if name.upper() not in regionList:
-                    continue
-            # Allow a fifth column containing indication of what type of region it is.
-            # Typically exon/intron designation. This will be deprecated.
-            feature = None if len(targetsplit) <= 4 else targetsplit[4]
-            self.targets.setdefault(name.upper(), [])
-            self.targets[name.upper()].append((chrm, int(bp1), int(bp2), name, feature))
-        # print 'Targets', self.targets
-        utils.log(self.loggingName, 'info', '%d targets' % len(self.targets))
+        with open(self.get_param('targets_bed_file'), 'rU') as target_bed_file:
+            for target in target_bed_file:
+                # Each target is formatted like a bed, chr bp1 bp2 name
+                target = target.strip()
+                targetsplit = target.split()
+                chrm, bp1, bp2, name = targetsplit[0:4]
+                if region_list:
+                    if name.upper() not in region_list:
+                        continue
+                # Allow a fifth column containing indication of what type of region it is.
+                # Typically exon/intron designation. This will be deprecated.
+                feature = None if len(targetsplit) <= 4 else targetsplit[4]
+                self.targets.setdefault(name.upper(), [])
+                self.targets[name.upper()].append((chrm, int(bp1), int(bp2), name, feature))
+        utils.log(self.logging_name, 'info', '%d targets' % len(self.targets))
 
     def get_param(self, key, required=False):
         """Get the parameter value in the self.opts dictionary.
@@ -220,9 +231,12 @@ class ParamManager:
         value = None
         if key in self.opts:
             value = self.opts[key]
-        elif required:
-            utils.log(self.loggingName, 'error', 'Missing required parameter %s, exiting.' % key)
+            if value == '':
+                value = None
+        if required and value is None:
+            utils.log(self.logging_name, 'error', 'Missing required parameter %s, exiting.' % key)
             sys.exit(1)
+        sys.stderr.write('Warning: %s is set to None\n' % key)
         return value
 
     def set_param(self, key, value):
