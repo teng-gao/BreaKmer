da1f4ce71056d3632e86a3b89f638f85b89c981e
ryan.abo@gmail.com <ryan.abo@gmail.com>
2016-05-24 11:29:50 -0400

    Refactored code and merged contig classes.

--- breakmer/assembly/contig.py
+++ breakmer/assembly/contig.py
@@ -1,22 +1,27 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+
+'''
+BreaKmer contig module
+'''
+
 import os
-import sys
 import shutil
 import pysam
 import logging
 import subprocess
-import breakmer.sv_caller as sv_caller
+# import breakmer.sv_caller as sv_caller
 import breakmer.utils as utils
 import breakmer.assembly.utils as assembly_utils
-# import breakmer.assembly.assembler as assembler
+# import breakmer.assembly.assembly_contig as assembly_contig
 import breakmer.assembly.olc as olc
 
 __author__ = "Ryan Abo"
 __copyright__ = "Copyright 2015, Ryan Abo"
 __email__ = "ryanabo@gmail.com"
 __license__ = "MIT"
+
 
 
 def get_read_kmers(seq, l, skmers): 
@@ -64,10 +69,10 @@ class read_batch:
     self.alt = []
     self.batch_reads = [b_read(read, False, True, True)]
     self.mer_pos_d = {mer_pos:[0]}
-  #*******************************************
+  
   def set_last_read_aligned(self):
     self.batch_reads[-1].aligned = True
-  #*******************************************
+  
   def clean(self, fq_reads, buff, last_keep_read):
     map(fq_reads.__delitem__, map(lambda x: x.seq, list(self.delete)))
     for read_id in filter(lambda x: x in buff.contigs, list(self.delete)):
@@ -77,7 +82,7 @@ class read_batch:
     self.alt = []
     self.batch_reads = [last_keep_read]
     self.mer_pos_d = {}
-  #*******************************************
+  
   def check_mer_read(self, pos, read):
     check = True
     redund_read = False
@@ -91,7 +96,7 @@ class read_batch:
         self.mer_pos_d[pos].append(len(self.batch_reads))
       self.batch_reads.append(b_read(read,False,check,False))
     return check 
-  #*******************************************
+  
 
 class assembly_counts:
   def __init__(self, read, nreads):
@@ -156,139 +161,6 @@ class assembly_counts:
         fill_counts.extend(self.indel_only)
         self.indel_only = fill_counts
 
-
-class TargetContig(object):
-
-  '''
-  '''
-
-  def __init__(self, parent_target, contig_id, assembly):
-    self.params = parent_target.params
-    self.id = contig_id
-    self.query_region = parent_target.get_values()
-    self.path = os.path.join(parent_target.paths['contigs'], contig_id)
-    self.assembly_fq_fn = os.path.join(parent_target.paths['contigs'], contig_id, contig_id+".fq")
-    self.reads = assembly.reads
-    self.kmers = assembly.kmers
-    self.contig_seq = assembly.get_contig_seq()
-    self.contig_rcounts = assembly.get_contig_counts()
-    self.contig_kmer_locs = assembly.get_kmer_locs()
-    self.result = None
-    self.contig_fa_fn = None
-    self.query_res_fn = None
-    self.logger = logging.getLogger('root')
-    self.setup(parent_target.files['kmer_clusters'])
-
-  def setup(self, cluster_fn):
-    self.logger.info('Setting up contig path %s'%self.path)
-    if not os.path.exists(self.path): os.makedirs(self.path)
-    self.write_cluster_file(cluster_fn)
-    self.write_read_fq()
-    self.write_contig_fa()
-
-  def write_cluster_file(self, cluster_fn):
-    cluster_f = open(cluster_fn, 'w')
-    cluster_f.write(self.id + " " + str(len(self.kmers)) + "\n")
-    cluster_f.write(",".join([x[0] for x in self.kmers]) + "\n")
-    cluster_f.write(",".join([x.id for x in self.reads]) + "\n\n")
-    cluster_f.close()
-      
-  def write_read_fq(self):
-    assembly_fq = open(self.assembly_fq_fn, 'w')
-    self.logger.info('Writing reads containing kmers to fastq %s'%self.assembly_fq_fn)
-    for read in self.reads:
-      assembly_fq.write(read.id+"\n"+read.seq+"\n+\n"+read.qual+"\n")
-    assembly_fq.close()
-
-  def write_contig_fa(self):
-    self.contig_fa_fn = os.path.join(self.path, self.id+".fa")
-    self.logger.info('Writing contig fasta file for blatting %s'%self.contig_fa_fn)
-    blat_f = open(self.contig_fa_fn, 'w')
-    blat_f.write(">contig1"+"\n"+self.contig_seq)
-    blat_f.close()
-
-  def has_result(self):
-    if self.result: return True
-    else: return False
-
-  def write_result(self, output_path):
-    res_fn = os.path.join(self.path,self.id + "_svs.out")
-    self.logger.info('Writing %s result file %s'%(self.id,res_fn))
-    if self.result: 
-      res_f = open(res_fn,'w')
-      res_f.write("\t".join([str(x) for x in self.result]))
-      res_f.close()
-      shutil.copyfile(res_fn, os.path.join(output_path, self.id+"_svs.out"))   
-   
-  def write_bam(self, bam_in, path):
-    bam_out_fn = os.path.join(path,self.id+"_reads.bam")
-    self.logger.info('Writing contig reads bam file %s'%bam_out_fn)
-    bam_out_sorted_fn = os.path.join(path,self.id+"_reads.sorted.bam")
-    bamf = pysam.Samfile(bam_in,'rb')
-    bam_out_f = pysam.Samfile(bam_out_fn,"wb",template=bamf)
-    for bam_read in bamf.fetch():
-      for read in self.reads:
-        rid, idx = read.id.lstrip("@").split("/")
-        ridx, indel_only_read = idx.split("_")
-        if (bam_read.qname == rid) and ((ridx=='2' and bam_read.is_read2) or (ridx=='1' and bam_read.is_read1)):
-          bam_out_f.write(bam_read)
-    bamf.close()
-    bam_out_f.close()
-    self.logger.info('Sorting bam file %s to %s'%(bam_out_fn,bam_out_sorted_fn))
-    pysam.sort(bam_out_fn,bam_out_sorted_fn.replace('.bam',''))
-    self.logger.info('Indexing bam file %s'%bam_out_sorted_fn)
-    pysam.index(bam_out_sorted_fn)      
-
-  def query_ref(self, target_ref_fn, query_region):
-    if self.contig_fa_fn:
-      self.run_blat(target_ref_fn, 'target') # Run blat against target reference sequence first for speed.
-      if not self.query_res_fn:
-        self.logger.info('No blat results file %s, no calls for %s.'%(self.query_res_fn, self.id))
-        return
-      if not self.check_target_blat(query_region):
-        # Blat against whole genome reference fasta
-        self.run_blat(self.params.opts['reference_fasta'], 'all')
-
-  def run_blat(self, db, name):
-    self.query_res_fn = os.path.join(self.path,'blat_res.'+name+'.psl')
-    if not os.path.isfile(self.query_res_fn):
-      if name == 'all': 
-        self.logger.info('Running blat %s, storing results in %s'%(self.params.opts['gfclient'],self.query_res_fn))
-        cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -nohead localhost %d %s %s %s'%(self.params.opts['gfclient'],self.params.opts['blat_port'], self.params.opts['reference_fasta_dir'], self.contig_fa_fn, self.query_res_fn)
-      else:
-        self.logger.info('Running blat %s, storing results in %s'%(self.params.opts['blat'],self.query_res_fn))
-        cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -stepSize=10 -minMatch=2 -repeats=lower -noHead %s %s %s'%(self.params.opts['blat'], db, self.contig_fa_fn, self.query_res_fn)
-#        cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -stepSize=1 -minMatch=1 -repeats=lower -noHead %s %s %s'%(self.params.opts['blat'], db, self.contig_fa_fn, self.query_res_fn)
-
-      self.logger.info('Blat system command %s'%cmd)
-      p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
-      output, errors = p.communicate()
-      self.logger.info('Blat output %s'%output)
-      if errors != '': self.logger.info('Blat errors %s'%errors)
-    else: self.logger.info('Blat already run, results file %s exists, continuing'%self.query_res_fn)
-
-  def check_target_blat(self, query_region ):
-    meta_dict = {'offset': query_region[1]-200, 'tname': query_region[0].replace('chr',''), 'query_res_fn': self.query_res_fn, 'sbam': self.params.opts['sample_bam_file'], 'query_region': query_region}
-    am = sv_caller.align_manager(meta_dict)
-    hit, self.query_res_fn = am.check_target_results()
-    return hit
-
-  def make_calls(self, query_region, disc_reads, rep_mask):
-    meta_dict = {'params': self.params,
-                 'repeat_mask': rep_mask,
-                 'query_region': query_region,
-                 'query_res_fn': self.query_res_fn,
-                 'disc_reads': disc_reads,
-                 'contig_vals': (self.contig_seq, self.contig_rcounts, self.id,self.reads, len(self.kmers), self.contig_kmer_locs),
-                 'sbam': self.params.opts['sample_bam_file']
-                }
-
-    print 'Making calls make_calls()'
-    print meta_dict
-    am = sv_caller.align_manager(meta_dict)
-    self.result = am.get_result()
-
-
 class AssemblySeq(object):
 
     '''
@@ -335,12 +207,12 @@ class AssemblySeq(object):
         #    self.counts = precounts
 
 
-class AssemblyContig(object):
+class Contig(object):
 
     '''
     '''
 
-    def __init__(self, kmer_unit, read_align_values): # kmer_val, read, mer_pos, nreads, kmer_len):
+    def __init__(self, kmer_unit, read_align_values):
         self.reads = set()
         self.seq = AssemblySeq(read_align_values['read'], read_align_values['nreads'])
         self.kmer_locs = []
@@ -351,6 +223,9 @@ class AssemblyContig(object):
         self.rb = read_batch(read_align_values['read'], read_align_values['align_pos'])
         self.logging_name = 'breakmer.assembly.contig'
         self.setup = False
+        self.contig_id = None
+        self.contig_fa_fn = None
+        self.file_path = None
 
     def grow(self, fq_recs, kmer_tracker, kmer_len, contig_buffer):
 
@@ -569,7 +444,7 @@ class AssemblyContig(object):
                         read_align_values = {'read': read,
                                              'align_pos': kmer_pos,
                                              'nreads': nreads}
-                        new_contigs.append((read, AssemblyContig(kmer_unit, read_align_values)))
+                        new_contigs.append((read, Contig(kmer_unit, read_align_values)))
                         mer_set = mer_set | x
                         break
         return new_contigs
@@ -606,3 +481,171 @@ class AssemblyContig(object):
 
         reads = assembly_utils.find_reads(mer, read_items, self.buffer, read_order)
         return reads
+
+    def write_contig_values(self, contig_id, cluster_fn, target_contig_path):
+
+        '''
+        '''
+
+        self.contig_id = contig_id
+        self.file_path = os.path.join(target_contig_path, contig_id)
+
+        utils.log(self.logging_name, 'info', 'Setting up contig path %s' % self.file_path)
+        if not os.path.exists(self.file_path):
+            os.makedirs(self.file_path)
+        self.write_cluster_file(cluster_fn)
+        self.write_read_fq(os.path.join(self.file_path, contig_id + ".fq"))
+        self.write_contig_fa()
+
+    def write_cluster_file(self, cluster_fn):
+        cluster_f = open(cluster_fn, 'w')
+        cluster_f.write(self.contig_id + " " + str(len(self.kmers)) + "\n")
+        cluster_f.write(",".join([x[0] for x in self.kmers]) + "\n")
+        cluster_f.write(",".join([x.id for x in self.reads]) + "\n\n")
+        cluster_f.close()
+      
+    def write_read_fq(self, assembly_fq_fn):
+        assembly_fq = open(assembly_fq_fn, 'w')
+        utils.log(self.logging_name, 'info', 'Writing reads containing kmers to fastq %s' % assembly_fq_fn)
+        for read in self.reads:
+            assembly_fq.write(read.id + "\n" + read.seq + "\n+\n" + read.qual + "\n")
+        assembly_fq.close()
+
+    def write_contig_fa(self):
+        self.contig_fa_fn = os.path.join(self.file_path, self.contig_id + ".fa")
+        utils.log(self.logging_name, 'info', 'Writing contig fasta file for blatting %s' % self.contig_fa_fn)
+        blat_file = open(self.contig_fa_fn, 'w')
+        blat_file.write(">" + self.contig_id + "\n" + self.seq.value)
+        blat_file.close()
+
+
+# class TargetContig(object):
+
+#   '''
+#   '''
+
+#   def __init__(self, parent_target, contig_id, assembly):
+#     self.params = parent_target.params
+#     self.id = contig_id
+#     self.query_region = parent_target.get_values()
+#     self.path = os.path.join(parent_target.paths['contigs'], contig_id)
+#     self.assembly_fq_fn = os.path.join(parent_target.paths['contigs'], contig_id, contig_id+".fq")
+#     self.reads = assembly.reads
+#     self.kmers = assembly.kmers
+#     self.contig_seq = assembly.get_contig_seq()
+#     self.contig_rcounts = assembly.get_contig_counts()
+#     self.contig_kmer_locs = assembly.get_kmer_locs()
+#     self.result = None
+#     self.contig_fa_fn = None
+#     self.query_res_fn = None
+#     self.logger = logging.getLogger('root')
+#     self.setup(parent_target.files['kmer_clusters'])
+
+#   def setup(self, cluster_fn):
+#     self.logger.info('Setting up contig path %s'%self.path)
+#     if not os.path.exists(self.path): os.makedirs(self.path)
+#     self.write_cluster_file(cluster_fn)
+#     self.write_read_fq()
+#     self.write_contig_fa()
+
+#   def write_cluster_file(self, cluster_fn):
+#     cluster_f = open(cluster_fn, 'w')
+#     cluster_f.write(self.id + " " + str(len(self.kmers)) + "\n")
+#     cluster_f.write(",".join([x[0] for x in self.kmers]) + "\n")
+#     cluster_f.write(",".join([x.id for x in self.reads]) + "\n\n")
+#     cluster_f.close()
+      
+#   def write_read_fq(self):
+#     assembly_fq = open(self.assembly_fq_fn, 'w')
+#     self.logger.info('Writing reads containing kmers to fastq %s'%self.assembly_fq_fn)
+#     for read in self.reads:
+#       assembly_fq.write(read.id+"\n"+read.seq+"\n+\n"+read.qual+"\n")
+#     assembly_fq.close()
+
+#   def write_contig_fa(self):
+#     self.contig_fa_fn = os.path.join(self.path, self.id+".fa")
+#     self.logger.info('Writing contig fasta file for blatting %s'%self.contig_fa_fn)
+#     blat_f = open(self.contig_fa_fn, 'w')
+#     blat_f.write(">contig1"+"\n"+self.contig_seq)
+#     blat_f.close()
+
+#   def has_result(self):
+#     if self.result: return True
+#     else: return False
+
+#   def write_result(self, output_path):
+#     res_fn = os.path.join(self.path,self.id + "_svs.out")
+#     self.logger.info('Writing %s result file %s'%(self.id,res_fn))
+#     if self.result: 
+#       res_f = open(res_fn,'w')
+#       res_f.write("\t".join([str(x) for x in self.result]))
+#       res_f.close()
+#       shutil.copyfile(res_fn, os.path.join(output_path, self.id+"_svs.out"))   
+   
+#   def write_bam(self, bam_in, path):
+#     bam_out_fn = os.path.join(path,self.id+"_reads.bam")
+#     self.logger.info('Writing contig reads bam file %s'%bam_out_fn)
+#     bam_out_sorted_fn = os.path.join(path,self.id+"_reads.sorted.bam")
+#     bamf = pysam.Samfile(bam_in,'rb')
+#     bam_out_f = pysam.Samfile(bam_out_fn,"wb",template=bamf)
+#     for bam_read in bamf.fetch():
+#       for read in self.reads:
+#         rid, idx = read.id.lstrip("@").split("/")
+#         ridx, indel_only_read = idx.split("_")
+#         if (bam_read.qname == rid) and ((ridx=='2' and bam_read.is_read2) or (ridx=='1' and bam_read.is_read1)):
+#           bam_out_f.write(bam_read)
+#     bamf.close()
+#     bam_out_f.close()
+#     self.logger.info('Sorting bam file %s to %s'%(bam_out_fn,bam_out_sorted_fn))
+#     pysam.sort(bam_out_fn,bam_out_sorted_fn.replace('.bam',''))
+#     self.logger.info('Indexing bam file %s'%bam_out_sorted_fn)
+#     pysam.index(bam_out_sorted_fn)      
+
+#   def query_ref(self, target_ref_fn, query_region):
+#     if self.contig_fa_fn:
+#       self.run_blat(target_ref_fn, 'target') # Run blat against target reference sequence first for speed.
+#       if not self.query_res_fn:
+#         self.logger.info('No blat results file %s, no calls for %s.'%(self.query_res_fn, self.id))
+#         return
+#       if not self.check_target_blat(query_region):
+#         # Blat against whole genome reference fasta
+#         self.run_blat(self.params.opts['reference_fasta'], 'all')
+
+#   def run_blat(self, db, name):
+#     self.query_res_fn = os.path.join(self.path,'blat_res.'+name+'.psl')
+#     if not os.path.isfile(self.query_res_fn):
+#       if name == 'all': 
+#         self.logger.info('Running blat %s, storing results in %s'%(self.params.opts['gfclient'],self.query_res_fn))
+#         cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -nohead localhost %d %s %s %s'%(self.params.opts['gfclient'],self.params.opts['blat_port'], self.params.opts['reference_fasta_dir'], self.contig_fa_fn, self.query_res_fn)
+#       else:
+#         self.logger.info('Running blat %s, storing results in %s'%(self.params.opts['blat'],self.query_res_fn))
+#         cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -stepSize=10 -minMatch=2 -repeats=lower -noHead %s %s %s'%(self.params.opts['blat'], db, self.contig_fa_fn, self.query_res_fn)
+# #        cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -stepSize=1 -minMatch=1 -repeats=lower -noHead %s %s %s'%(self.params.opts['blat'], db, self.contig_fa_fn, self.query_res_fn)
+
+#       self.logger.info('Blat system command %s'%cmd)
+#       p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+#       output, errors = p.communicate()
+#       self.logger.info('Blat output %s'%output)
+#       if errors != '': self.logger.info('Blat errors %s'%errors)
+#     else: self.logger.info('Blat already run, results file %s exists, continuing'%self.query_res_fn)
+
+#   def check_target_blat(self, query_region ):
+#     meta_dict = {'offset': query_region[1]-200, 'tname': query_region[0].replace('chr',''), 'query_res_fn': self.query_res_fn, 'sbam': self.params.opts['sample_bam_file'], 'query_region': query_region}
+#     am = sv_caller.align_manager(meta_dict)
+#     hit, self.query_res_fn = am.check_target_results()
+#     return hit
+
+  # def make_calls(self, query_region, disc_reads, rep_mask):
+  #   meta_dict = {'params': self.params,
+  #                'repeat_mask': rep_mask,
+  #                'query_region': query_region,
+  #                'query_res_fn': self.query_res_fn,
+  #                'disc_reads': disc_reads,
+  #                'contig_vals': (self.contig_seq, self.contig_rcounts, self.id,self.reads, len(self.kmers), self.contig_kmer_locs),
+  #                'sbam': self.params.opts['sample_bam_file']
+  #               }
+
+  #   print 'Making calls make_calls()'
+  #   print meta_dict
+  #   am = sv_caller.align_manager(meta_dict)
+  #   self.result = am.get_result()
